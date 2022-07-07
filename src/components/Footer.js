39cffa088dec2e24c68e264c0d3f74610b8c470c
mnogueron <matthieu.nogueron@live.fr>
2022-07-07 15:05:05 +0200

    feat: upgrade footer sections to better reflect site content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,139 +1,127 @@
-import React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import {Heading} from "@chakra-ui/react";
+import React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import {
+  Flex,
+  Box,
+  Heading,
+  Stack,
+  VStack,
+  Link,
+  Text,
+  Divider,
+} from '@chakra-ui/react';
+
+const SectionHeader = ({ children, ...rest }) => (
+  <Heading
+    as="h3"
+    size="sm"
+    textTransform="uppercase"
+    color="gray.600"
+    fontWeight="semibold"
+    textAlign={{ base: 'center', md: 'left' }}
+    {...rest}
+  >
+    {children}
+  </Heading>
+);
+
+const FooterLink = ({ to, children }) => (
+  <Link
+    as={GatsbyLink}
+    to={to}
+    fontSize="sm"
+    color="gray.600"
+  >
+    {children}
+  </Link>
+);
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100">
+    <Box as="footer" className="bg-gray-100">
       <div className="max-w-7xl px-4 py-4 mx-auto sm:px-6 lg:px-8">
-        <div className="lg:flex">
-          <div className="w-full -mx-4 sm:-mx-6 lg:-mx-8 lg:w-2/5">
-            <div className="px-4 sm:px-6 lg:px-8">
-              <Link className="flex items-center gap-1" to="/">
-                <StaticImage
-                  src="../img/bml-icon.png"
-                  alt="Badminton Maisons-Laffitte icon"
-                  layout="fixed"
-                  width={114}
-                  height={85}
-                  backgroundColor="transparent"
-                  placeholder="blurred"
-                />
-                <Heading size={'sm'}>Badminton Maisons-Laffitte</Heading>
-                {/*<div className="text-xl font-bold tracking-wide">
-                  <span className="text-gray-800 font-semibold">Urban</span>
-                  <span className="text-green-700">Garden</span>
-                </div>*/}
-              </Link>
-            </div>
-          </div>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          spacing={{ base: 4, md: 8 }}
+        >
+          <Box mx="auto">
+            <Link className="flex items-center gap-1" to="/">
+              <StaticImage
+                src="../img/bml-icon.png"
+                alt="Badminton Maisons-Laffitte icon"
+                layout="fixed"
+                width={114}
+                height={85}
+                backgroundColor="transparent"
+                placeholder="blurred"
+              />
+              <Heading size={'sm'}>Badminton Maisons-Laffitte</Heading>
+            </Link>
+          </Box>
 
-          <div className="mt-6 lg:mt-0 lg:flex-1">
-            <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
-              <div>
-                <h3 className="text-gray-700 uppercase dark:text-white">
-                  About
-                </h3>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
+          <Box mx="auto !important">
+            <Stack
+              direction={{ base: 'column', md: 'row' }}
+              spacing={{ base: 6, md: 12 }}
+            >
+              <Box>
+                <SectionHeader>Infos Pratiques</SectionHeader>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  spacing={{ base: 2, sm: 20, md: 10 }}
+                  mt={2}
                 >
-                  Company
-                </Link>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Community
-                </Link>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Careers
-                </Link>
-              </div>
-
-              <div>
-                <h3 className="text-gray-700 uppercase dark:text-white">
-                  Blog
-                </h3>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Tec
-                </Link>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Music
-                </Link>
-                <Link
-                  to="/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                >
-                  Videos
-                </Link>
-              </div>
-
-              <div>
-                <h3 className="text-gray-700 uppercase dark:text-white">
-                  Products
-                </h3>
-                <a
-                  href="https://www.gatsbyjs.com/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Gatsby
-                </a>
-                <a
-                  href="https://www.netlifycms.org/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Netlify CMS
-                </a>
-                <a
-                  href="https://tailwindcss.com/"
-                  className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Tailwind CSS
-                </a>
-              </div>
-
-              <div>
-                <h3 className="text-gray-700 uppercase dark:text-white">
-                  Contact
-                </h3>
-                <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
+                  <VStack spacing={2} alignItems={{base: 'center', sm: "flex-start"}}>
+                    <FooterLink to="/infos-pratiques/sections">
+                      Nos Sections
+                    </FooterLink>
+                    <FooterLink to="/infos-pratiques/inscription">
+                      Comment s'inscrire
+                    </FooterLink>
+                    <FooterLink to="/infos-pratiques/entrainements">
+                      Les entraînements
+                    </FooterLink>
+                  </VStack>
+                  <VStack spacing={2} alignItems={{base: 'center', sm: "flex-start"}}>
+                    <FooterLink to="/infos-pratiques/avantages">
+                      Les avantages du club
+                    </FooterLink>
+                    <FooterLink to="/infos-pratiques/acces-horaires">
+                      Accès / Horaires
+                    </FooterLink>
+                  </VStack>
+                </Stack>
+              </Box>
+              <Box>
+                <SectionHeader>Contact</SectionHeader>
+                <VStack spacing={2} alignItems={{base: 'center', md: "flex-start"}} mt={2}>
+                  {/*<span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
                   +1 526 654 8965
-                </span>
-                <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
-                  example@email.com
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+                </span>*/}
+                  <Link
+                    href={'mailto:club.badml@gmail.com'}
+                    fontSize="sm"
+                    color="gray.600"
+                  >
+                    club.badml@gmail.com
+                  </Link>
+                </VStack>
+              </Box>
+            </Stack>
+          </Box>
+        </Stack>
 
-        <hr className="h-px my-6 bg-gray-300 border-none dark:bg-gray-700" />
+        <Divider borderColor={'gray.400'} my={6} />
 
         <div>
-          <p className="text-center text-gray-800 dark:text-white">
-            © BML 2022 - All rights reserved
-          </p>
+          <Text textAlign="center" color="gray.700" fontSize="sm">
+            © BML 2022 - Tous Droits Réservés - All Rights Reserved
+          </Text>
         </div>
       </div>
-    </footer>
-  )
-}
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;

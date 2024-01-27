dbc47621a788cddaa379734312ca922b2199a7e7
mnogueron <matthieu.nogueron@live.fr>
2024-01-27 23:45:46 +0100

    fix: disable dnt

--- gatsby-config.js
+++ gatsby-config.js
@@ -186,7 +186,7 @@ module.exports = {
           // Puts tracking script in the head instead of the body
           head: true,
           // Setting this parameter is also optional
-          respectDNT: true,
+          respectDNT: false,
           // Avoids sending pageview hits from custom paths
           //exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com

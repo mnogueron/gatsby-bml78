0f4fd1165e8c84f6e5faa820b2e0ce4d450e7e5b
mnogueron <matthieu.nogueron@live.fr>
2024-05-13 23:16:41 +0200

    fix: use decap cms gatsby plugin straight from npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,7 +149,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-netlify-cms`,
+      resolve: `gatsby-plugin-decap-cms`,
       options: {
         /**
          * One convention is to place your Netlify CMS customization code in a

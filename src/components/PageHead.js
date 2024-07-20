612bcb413850a62abe75cabedc588d64a6acec5c
mnogueron <matthieu.nogueron@live.fr>
2024-07-21 01:56:37 +0200

    fix: embed youtube videos woth nocoookies

--- src/components/PageHead.js
+++ src/components/PageHead.js
@@ -50,11 +50,11 @@ const PageHead = ({data, pageContext}) => {
 
   return (
     <>
+      <html lang="fr" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
       />
-      <html lang="en" />
 
       <title>{seo.titleTemplate.replace('%s', seo.title)}</title>
 
--- src/containers/Home/components/YoutubeVideo.js
+++ src/containers/Home/components/YoutubeVideo.js
@@ -6,11 +6,12 @@ const YoutubeVideo = ({id}) => {
     <AspectRatio ratio={16 / 9}>
       <Box
         as="iframe"
-        src={`https://www.youtube.com/embed/${id}`}
+        src={`https://www.youtube-nocookie.com/embed/${id}`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
+        referrerpolicy="strict-origin-when-cross-origin"
         borderRadius={10}
       />
     </AspectRatio>

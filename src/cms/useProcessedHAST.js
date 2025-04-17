9ec41448e26abaf6f3fc6c075f6c7602e395dd33
mnogueron <matthieu.nogueron@gmail.com>
2025-04-17 17:16:20 +0200

    fix: remove missing dep with katex

--- src/cms/useProcessedHAST.js
+++ src/cms/useProcessedHAST.js
@@ -3,9 +3,7 @@ import {unified} from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import remarkFrontmatter from 'remark-frontmatter';
-import remarkMath from 'remark-math';
 import rehypeRaw from 'rehype-raw';
-import katex from 'rehype-katex';
 
 const useProcessedHAST = data => {
   const [processedHAST, setProcessedHAST] = useState(null);
@@ -14,9 +12,7 @@ const useProcessedHAST = data => {
     const prepareHTML = async () => {
       const processor = unified()
         .use(remarkParse)
-        .use(remarkMath)
         .use(remarkRehype, {allowDangerousHtml: true})
-        .use(katex)
         .use(remarkFrontmatter)
         .use(rehypeRaw);
       const preparedHTML = await processor.run(processor.parse(data));
